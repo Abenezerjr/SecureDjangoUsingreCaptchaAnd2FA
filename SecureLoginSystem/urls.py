c5ab268de1571e5c8f8dacf287c9b792a2086086
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-05-17 23:53:42 +0300

    start working with security of the apps

--- SecureLoginSystem/urls.py
+++ SecureLoginSystem/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('register',views.Register ,name='register'),
+    path('dashboard',views.dashboard,name='dashboard'),
+    path('login',views.login_user,name='login'),
 ]
--- SecureLoginSystem/views.py
+++ SecureLoginSystem/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .forms import CreateUserForm
 # Create your views here.
 
 
 def home(request):
-    return render(request,'SecureLoginSystem/index.html')
+    return render(request, 'home.html')
+
+def Register(request):
+    form=CreateUserForm()
+    if request.method=='POST':
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context={
+        'form':form
+    }
+    return render(request,'SecureLoginSystem/register.html',context)
+
+def dashboard(request):
+    return render(request,'SecureLoginSystem/dashboard.html')
+
+def login_user(request):
+    return render(request,'SecureLoginSystem/login.html')
